a0e18e200d02b5e7082b99b581ce441a63d60d35
Yevhen <galitsyn.evgeniy955@gmail.com>
2025-03-25 20:50:02 +0200

    if else

---
+++ if_else.js
@@ -0,0 +1,34 @@
+let number = 15;
+
+// if (number === 10) {
+//     console.log("Число равно 15");
+// } else {
+//     console.log("Число не равно 15");
+// }
+
+// let score = 75;
+//
+// if (score >= 90) {
+//     console.log("Отлично");
+// } else if (score >= 70) {
+//     console.log("Хорошо");
+// } else if (score >= 50) {
+//     console.log("Удовлетворительно");
+// } else {
+//     console.log("Плохо");
+// }
+
+// let age = 17.5;
+// let access = age >= 18 ? "Доступ разрешен" : "Доступ запрещен";
+// console.log(access);
+
+let score2 = 70;
+
+if (score2 >= 90) {
+    console.log("Отлично");
+} else if (score2 !== 70) {
+    console.log("Хорошо");
+} else {
+    console.log("Плохо");
+    console.log(score2);
+}
--- types_js.js
+++ types_js.js
@@ -17,3 +17,15 @@ console.log("Переменая: ", typeof num3);
 let num4 = 5;
 let num5 = "7";
 console.log(Number(num4 + num5));
+
+let num6 = 50;
+let num7 = 48;
+console.log("Результат: ", num6 % num7);
+
+let num8 = 1;
+num8 += 2;
+console.log("Результат: ", num8);
+num8++;
+console.log("Результат: ", num8);
+num8--;
+console.log("Результат: ", String(num8));
