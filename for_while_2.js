4b67d74097ad71c53f1344da8483e35692284088
Yevhen <galitsyn.evgeniy955@gmail.com>
2025-03-28 00:09:18 +0200

    for while do while

---
+++ for_while_2.js
@@ -0,0 +1,12 @@
+for (let i = 10; i <=20; i+= 2) {
+    if (i > 15)
+        break;
+    console.log(i)
+}
+
+for (let p = 10; p <=20; p++) {
+    if (p % 2 === 0)
+        continue
+    console.log(p)
+}
+
