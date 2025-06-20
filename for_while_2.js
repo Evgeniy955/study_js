4e83c652a60edbc9b10e1d09cf33c4d379a1ce78
Yevhen <galitsyn.evgeniy955@gmail.com>
2025-06-20 16:29:41 +0300

    Refactor array manipulation to use forEach and for...of loops

--- for_while_2.js
+++ for_while_2.js
@@ -14,6 +14,24 @@ for (let p = 10; p <=20; p++) {
 
 let arr = [5, 7, 3, 8, 9, "Hello"];
 for (let i = 0; i < arr.length; i++) {
-    arr[i] *= 2;
+    arr[i] *= 2;kl
     console.log("Элемент " + (i+ 1) + ": " + arr[i]);
+}
+
+let arr = [5, 7, 3, 8, 9, "Hello"];
+arr.forEach((item, i, array) => {
+    if (typeof item === "number") {
+        array[i] = item * 2;
+    }
+    console.log("Элемент " + (i + 1) + ": " + array[i]);
+});
+
+let arr2 = [5, 7, 3, 8, 9, "Hello"];
+let index = 0;
+for (let value of arr2) {
+    if (typeof value === "number") {
+        arr2[index] = value * 2;
+    }
+    console.log("Элемент " + (index + 1) + ": " + arr2[index]);
+    index++;
 }
